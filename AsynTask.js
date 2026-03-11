546a509667d8df5687c8df0b883e1f66c561c768
samruddhimendgudle <samruddhimendgudle@gmail.com>
2026-03-11 12:19:27 +0530

    promise

--- AsynTask.js
+++ AsynTask.js
@@ -3,3 +3,16 @@ setTimeout(()=>{
     console.log("B")
 },4000)
 console.log("C")
+
+
+const promise=fetch("https://jsonplaceholder.typicode.com/posts")
+// promise.then((response)=>{
+//     return response.json()      
+// })
+
+promise.then((data)=>data.json())
+.then((actualdata)=>{
+    console.log(actualdata)
+}).catch((error)=>{
+    console.log(error)
+})
