c7c30a4d7d993a961cd8c3c5d495a7b3073113f8
samruddhimendgudle <samruddhimendgudle@gmail.com>
2026-03-13 11:42:23 +0530

    Spread operator

--- Arraydestructuring.js
+++ Arraydestructuring.js
@@ -7,3 +7,22 @@ let[c,d,...other]=cities
 console.log(c);
 console.log(d);
 console.log(other);
+
+let person={
+    name:"John",
+    age:30,
+    city:"New York",
+    
+}
+let{name,age}=person
+console.log(name);
+console.log(age);
+
+let cities1=["pune","Mumbai","nashik"];
+let cities2=["Banglore","Chennai","Hyderabad"];
+
+//ways to concat arrays
+let allCities=cities1+cities2
+let allCities1=[...cities1,...cities2] //spread operator
+let allCities2=cities1.concat(cities2)
+console.log(allCities);
