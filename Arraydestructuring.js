5f07479bdc48bc79ea83b84079e12830a79cd34b
samruddhimendgudle <samruddhimendgudle@gmail.com>
2026-03-13 11:31:06 +0530

    Array destructuring

---
+++ Arraydestructuring.js
@@ -0,0 +1,9 @@
+let cities=["pune","Mumbai","Banglore","Chennai","Hyderabad","Kolkata","Lucknow","Indore","Bhopal","Jaipur"];  
+let[a,b]=cities
+// console.log(a);
+// console.log(b);
+
+let[c,d,...other]=cities
+console.log(c);
+console.log(d);
+console.log(other);
